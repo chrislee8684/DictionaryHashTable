b15dca9eb56463c127be1154017c5c961237e97d
Chris Lee <Kanghyuk.lee@cooper.edu>
2022-10-02 14:37:51 -0400

    finalized version

---
+++ cmake-build-debug/CMakeFiles/DictionaryHashTable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DictionaryHashTable.dir/hash.cpp.o"
+  "CMakeFiles/DictionaryHashTable.dir/spellcheck.cpp.o"
+  "DictionaryHashTable"
+  "DictionaryHashTable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DictionaryHashTable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ spellcheck.cpp
@@ -0,0 +1,111 @@
+#include <fstream>
+#include <string>
+#include "hash.h"
+#include <cstdio>
+#include <cctype>
+#include <algorithm>
+#include <iomanip>
+
+int spellcheck(hashTable hashtable, std::string input_document, std::string output_file); //spellcheck func declaration
+
+int main(){
+    std::string input_dictionary;
+    std::string input_document;
+    std::string output_file;
+    hashTable hashtable(50000);
+
+    time_t start,end; //time function (returns in seconds)
+    int start_time = 0;
+    int end_time =0;
+
+    printf("Input Dictionary File: ");
+    std::cin>>input_dictionary;
+
+    std::ifstream dictionary; //declare stream object
+    dictionary.open(input_dictionary); //associate input_dictionary with dictionary object
+
+    printf("Input Document File: ");
+    std::cin>>input_document;
+
+    printf("Output File: ");
+    std::cin>>output_file;
+
+    //load dictionary
+    std::string line;
+    if(dictionary.is_open()){
+
+        start_time = time(&start);
+        while(!dictionary.eof()){
+            dictionary >> line;
+            std::transform(line.begin(),line.end(), line.begin(),
+                           [](unsigned char c){ return std::tolower(c); }); //turn string to lowercase
+            hashtable.insert(line);
+        }
+        end_time = time(&end);
+        std::cout<<"Dictionary loading time: " << std::setprecision(4) <<(end_time)-double(start_time)/CLOCKS_PER_SEC<<"seconds"<<std::endl;
+    }
+    else{
+        printf("The dictionary file cannot be opened.\n");
+        return EXIT_FAILURE;
+    }
+    dictionary.close(); //close dictionary
+
+    //spell check
+    start_time = time(&start);
+    spellcheck(hashtable, input_document, output_file);
+    end_time = time(&end);
+
+    std::cout<<"Spellcheck time: "<<  std::setprecision(4) << double(end_time)-double(start_time)/CLOCKS_PER_SEC<<"seconds"<<std::endl;
+    return 0;
+}
+
+int spellcheck(hashTable hashtable, std::string input_document, std::string output_file) {
+    std::string temp_string;
+    std::string temp_word;
+    int line_num = 0;
+    bool has_num = false;
+
+    std::ifstream document; //declare stream object
+    document.open(input_document); //associate input_dictionary with dictionary object
+
+    std::ofstream outputfile; //declare stream object
+    outputfile.open(output_file); //associate input_dictionary with dictionary object
+
+    if (document.is_open()) {
+        while (std::getline(document, temp_string)) {
+            line_num++;
+            temp_word = "";
+            has_num = false;
+
+            for (int i = 0; i < temp_string.size() + 1; i++) {
+                char c = tolower(temp_string[i]);
+
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') ||
+                    c == 39 || c == 45) { //compare ascii values
+                    if (c >= '0' && c <= '9') { //spec. check if contains number
+                        has_num = true;
+                    }
+                    temp_word += c;
+                } else {
+                    if (temp_word != "") {
+                        if (temp_word.size() > 20) { //larger than 20 chars
+                            outputfile << "Long word at line " << line_num << ", starts: " << temp_word.substr(0, 20)
+                                       << std::endl;
+                        } else if ((!hashtable.contains(temp_word)) && !has_num) {
+                            outputfile << "Unknown word at line " << line_num << ": " << temp_word << std::endl;
+                        }
+                    }
+                    temp_word = "";
+                    has_num = false;
+                }
+            }
+        }
+    } else {
+        printf("The document file cannot be opened.\n");
+        return EXIT_FAILURE;
+    }
+    document.close(); //close dictionary
+    outputfile.close();
+
+    return 0;
+}
